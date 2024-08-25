abe49e79b9afeb862d6d2c114ec0d0fd652a34fa
tharaka911 <tharakahitibandara@gmail.com>
2024-08-25 15:39:39 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,21 +8,17 @@ read zabbix_server
 echo "Please enter the hostname for this machine:"
 read hostname
 
-# Install required packages
-sudo apt-get update
-sudo apt-get install -y wget
-
 # Download the Zabbix agent 2 package for Debian based systems
-wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix/zabbix-agent2_6.0.0-1%2Bubuntu20.04_amd64.deb
+sudo wget https://repo.zabbix.com/zabbix/7.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_7.0-2+ubuntu24.04_all.deb
 
 # Install the Zabbix repository configuration package
-sudo dpkg -i zabbix-agent2_6.0.0-1+ubuntu20.04_amd64.deb
+sudo dpkg -i zabbix-release_7.0-2+ubuntu24.04_all.deb
 
 # Update package lists
-sudo apt-get update
+sudo apt update
 
 # Install the Zabbix agent 2 package
-sudo apt-get install -y zabbix-agent2
+sudo apt install zabbix-agent2 zabbix-agent2-plugin-*
 
 # Backup the original Zabbix agent 2 configuration file
 sudo cp /etc/zabbix/zabbix_agent2.conf /etc/zabbix/zabbix_agent2.conf.bak
@@ -34,12 +30,5 @@ sudo sed -i "s/Hostname=Zabbix server/Hostname=$hostname/g" /etc/zabbix/zabbix_a
 
 # Restart the Zabbix agent 2 service
 sudo systemctl restart zabbix-agent2
-
-# Enable the Zabbix agent 2 service to start on boot
 sudo systemctl enable zabbix-agent2
 
-# Add zabbix user to docker group 
-sudo usermod -aG docker zabbix
-
-# Change timezone 
-sudo timedatectl set-timezone Asia/Seoul
